6688525f3a377b75a0d9d71fbbe04d4306c712a3
Luk <luk.imperinetti@gmail.com>
2024-04-13 20:02:31 +0200

    first commit

---
+++ main.lua
@@ -0,0 +1,25 @@
+function love.load()
+    -- Initialize game resources and variables here
+    local screeWidth = love.graphics.getWidth()
+    local screeHeight = love.graphics.getHeight()
+end
+
+function love.update(dt)
+    -- Update game logic here
+end
+
+function love.draw()
+    -- Draw game graphics here
+end
+
+function love.keypressed(key)
+    -- Handle key presses here
+end
+
+function love.mousepressed(x, y, button)
+    -- Handle mouse presses here
+end
+
+function love.quit()
+    -- Clean up resources and save data here
+end
