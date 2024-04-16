f83546c88ae1111a98970d2778857b50ce369875
Luk <luk.imperinetti@gmail.com>
2024-04-16 22:23:21 +0200

    zombies randomly moving

--- main.lua
+++ main.lua
@@ -1,31 +1,71 @@
--- Renvoie l'angle entre deux vecteurs supposant la même origine.
-function math.angle(x1, y1, x2, y2) return math.atan(y2 - y1, x2 - x1) end
+-- Returns the angle between two vectors assuming the same origin.
+function math.angle(x1, y1, x2, y2) return math.atan2(y2 - y1, x2 - x1) end
 
--- Retourne la distance entre deux points
+-- Returns the distance between two points
 function math.dist(x1, y1, x2, y2) return ((x2 - x1) ^ 2 + (y2 - y1) ^ 2) ^ 0.5 end
 
 -- Initialize player & entities
 local lstSprites = {}
 local human = {}
 
-function CreateZombies(pList, pType, pImageFile, pFrame)             -- pList: find in lstSprites, pType: wich type? Zombie, human..., pImageFile: name of the file, pFrame: number of frames (player move etc..), pNumber: number of zombies
-    local zombie = CreateSprite(lstSprites, 'zombie', 'monster_', 2) -- became pList, pType, pImageFile, pFrame, pNumber
-    zombie.x = math.random(10, screeWidth - 10)
-    zombie.y = math.random(10, (screeHeight / 2) - 10)
+local ZSTATE = {}
+ZSTATE.NONE = ""
+ZSTATE.WALK = "walk"
+ZSTATE.ATTACK = "attack"
+ZSTATE.BITE = "bite"
+ZSTATE.CHANGEDIR = "changedir"
+
+-- Create Zombies
+function CreateZombies(pList, pType, pImageFile, pFrame)
+    zombie = CreateSprite(lstSprites, 'zombie', 'monster_', 2)
+    zombie.x = math.random(10, screenWidth - 10)
+    zombie.y = math.random(10, (screenHeight / 2) - 10)
 
     zombie.speed = math.random(5, 50) / 100
 
-    local angle = math.angle(zombie.x, zombie.y, human.x, human.y)
-    zombie.vx = zombie.speed * 60 * math.cos(angle)
-    zombie.vy = zombie.speed * 60 * math.cos(angle)
+    zombie.state = ZSTATE.NONE
 end
 
--- Sprite generation for Zombie & Human
-function CreateSprite(pList, pType, pImageFile, pFrame) -- pList: find in lstSprites, pType: wich type? Zombie, human..., pImageFile: name of the file, pFrame: number of frames (player move etc..)
+function UpdateZombies(pZombie)
+    if pZombie.state == ZSTATE.NONE then
+        pZombie.state = ZSTATE.CHANGEDIR
+    elseif pZombie.state == ZSTATE.WALK then
+        local collide = false
+        if pZombie.x < 0 then
+            pZombie.x = 0
+            collide = true
+        end
+        if pZombie.x > screenWidth then
+            pZombie.x = screenWidth
+            collide = true
+        end
+        if pZombie.y < 0 then
+            pZombie.y = 0
+            collide = true
+        end
+        if pZombie.y > screenHeight then
+            pZombie.y = screenHeight
+            collide = true
+        end
+        if collide then
+            pZombie.state = ZSTATE.CHANGEDIR
+        end
+    elseif pZombie.state == ZSTATE.ATTACK then
+    elseif pZombie.state == ZSTATE.CHANGEDIR then
+        local angle = math.angle(pZombie.x, pZombie.y, math.random(10, screenWidth - 10),
+            math.random(10, screenHeight - 10))
+        pZombie.vx = pZombie.speed * 60 * math.cos(angle)
+        pZombie.vy = pZombie.speed * 60 * math.sin(angle)
+        pZombie.state = ZSTATE.WALK
+    end
+end
+
+-- Create Sprite
+function CreateSprite(pList, pType, pImageFile, pFrame)
     local mySprite = {}
     mySprite.type = pType
-    mySprite.images = {}      -- store my entity frames
-    mySprite.currentFrame = 1 -- choose the first frame (image) as defaults
+    mySprite.images = {}
+    mySprite.currentFrame = 1
 
     for i = 1, pFrame do
         local fileName = "src/img/" .. pImageFile .. tostring(i) .. ".png"
@@ -44,44 +84,45 @@ function CreateSprite(pList, pType, pImageFile, pFrame) -- pList: find in lstSpr
     return mySprite
 end
 
+-- Main functions
 function love.load()
-    -- Initialize game resources and variables here
-    screeWidth = love.graphics.getWidth() / 2
-    screeHeight = love.graphics.getHeight() / 2
+    screenWidth = love.graphics.getWidth() / 2
+    screenHeight = love.graphics.getHeight() / 2
 
-    human = CreateSprite(lstSprites, 'human', 'player_', 4) -- became pList, pType, pImageFile, pFrame
-    human.x = screeWidth / 2
-    human.y = (screeHeight / 2) + (screeHeight / 4)
+    human = CreateSprite(lstSprites, 'human', 'player_', 4)
+    human.x = screenWidth / 2
+    human.y = (screenHeight / 2) + (screenHeight / 4)
 
     local nZombies
     for nZombies = 1, 15 do
-        CreateZombies(lstSprites, 'zombie', 'monster_', 2) -- became pList, pType, pImageFile, pFrame (10 zombies)
+        CreateZombies(lstSprites, 'zombie', 'monster_', 2)
     end
 end
 
 function love.update(dt)
-    -- Update game logic here
-
     for i, sprite in ipairs(lstSprites) do
         sprite.currentFrame = sprite.currentFrame + 0.1
         if sprite.currentFrame > #sprite.images + 1 then
             sprite.currentFrame = 1
         end
-        -- velocity
-        sprite.x = sprite.x + sprite.vx
-        sprite.y = sprite.y + sprite.vy
+        sprite.x = sprite.x + sprite.vx * dt
+        sprite.y = sprite.y + sprite.vy * dt
+
+        if sprite.type == 'zombie' then
+            UpdateZombies(sprite)
+        end
     end
 
-    if love.keyboard.isDown("q") then -- go left
+    if love.keyboard.isDown("q") then
         human.x = human.x - 1
     end
-    if love.keyboard.isDown("z") then -- go up
+    if love.keyboard.isDown("z") then
         human.y = human.y - 1
     end
-    if love.keyboard.isDown("d") then -- go right
+    if love.keyboard.isDown("d") then
         human.x = human.x + 1
     end
-    if love.keyboard.isDown("s") then -- go down
+    if love.keyboard.isDown("s") then
         human.y = human.y + 1
     end
 end
@@ -99,13 +140,10 @@ function love.draw()
 end
 
 function love.keypressed(key)
-    -- Handle key presses here
 end
 
 function love.mousepressed(x, y, button)
-    -- Handle mouse presses here
 end
 
 function love.quit()
-    -- Clean up resources and save data here
 end
